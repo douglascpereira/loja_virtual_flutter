1de27dfbe5bfe88c4a6350113fe3b90826b3d99d
douglaspereira <douglas.pereira@serpro.gov.br>
2019-10-14 16:33:41 -0300

    atualizacao tela de produto

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -12,7 +12,10 @@ class ProductScreen extends StatefulWidget {
 }
 
 class _ProductScreenState extends State<ProductScreen> {
+
   final ProductData product;
+
+  String size;
 
   _ProductScreenState(this.product);
 
@@ -75,12 +78,19 @@ class _ProductScreenState extends State<ProductScreen> {
                       childAspectRatio: 0.5,
                     ),
                     children: product.sizes.map((s) {
-                      GestureDetector(
+                      return GestureDetector(
+                        onTap: (){
+                          setState(() {
+                            size = s;
+                          });
+                        },
                         child: Container(
                           decoration: BoxDecoration(
                               borderRadius:
                                   BorderRadius.all(Radius.circular(4.0)),
-                              border: Border.all(color: Colors.grey[500])),
+                              border: Border.all(
+                                  color: s == size ? primaryColor : Colors.grey[500],
+                                width: 3.0 )),
                           width: 50.0,
                           alignment: Alignment.center,
                           child: Text(s),
@@ -88,6 +98,26 @@ class _ProductScreenState extends State<ProductScreen> {
                       );
                     }).toList(),
                   ),
+                ),
+                SizedBox(height: 16.0,),
+                SizedBox(
+                  height: 44.0,
+                  child: RaisedButton(
+                    onPressed: size != null ? (){} : null,
+                    child: Text("Adicionar ao Carrinho",
+                    style: TextStyle(fontSize: 18.0),),
+                    color: primaryColor,
+                    textColor: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 16.0,),
+                Text(
+                  "Descrição",
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  product.description,
+                  style: TextStyle(fontSize: 16.0),
                 )
               ],
             ),
