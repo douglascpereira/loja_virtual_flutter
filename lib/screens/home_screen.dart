f1ea2eb038459d7aaa02647242dab7e6a75f4ef8
Douglas <douglascpereira@gmail.com>
2019-10-13 21:57:24 -0300

    projeto loja online em flutter

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/tabs/products_tab.dart';
+import 'package:loja_virtual/widgets/custom_drawer.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    final _pageController = PageController();
+
+    return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Scaffold(
+          body: HomeTab(),
+          drawer: CustomDrawer(_pageController),
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Produtos"),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
+        ),
+        Container(color: Colors.yellow,),
+        Container(color: Colors.green,),
+      ],
+    );
+  }
+}
